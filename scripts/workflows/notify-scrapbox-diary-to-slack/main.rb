4ee3bb96ecbfe46998e5d48f3fdc683ccb51ec9c
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2021-05-15 09:42:21 +0900

    祝日のタグを付ける

--- scripts/workflows/notify-scrapbox-diary-to-slack/main.rb
+++ scripts/workflows/notify-scrapbox-diary-to-slack/main.rb
@@ -1,15 +1,26 @@
+require 'holiday_jp'
 require 'slack/incoming/webhooks'
 require 'uri'
 
-def scrapbox_diary_url(time)
+def scrapbox_diary_url(date)
+  tags = %w[%Y-%m %Y年%m月 %m-%d %m月%d日].map{ |format| date.strftime(format) }
+
+  tags += ['祝日', holiday_name(date)] if holiday_name(date)
+
   URI.parse('https://scrapbox.io').tap do |uri|
-    uri.path = '/tanaken0515/' + time.strftime('%F')
+    uri.path = '/tanaken0515/' + date.strftime('%F')
     uri.query = URI.encode_www_form({
-      body: %w[%Y-%m %Y年%m月 %m-%d %m月%d日].map{ |format| '#' + time.strftime(format) }.join(' ')
+      body: tags.map{ |tag| '#' + tag }.join(' ')
     })
   end.to_s
 end
 
-slack = Slack::Incoming::Webhooks.new(ENV['SLACK_WEBHOOK_URL'])
+def holiday_name(date)
+  HolidayJp.between(date, date).first&.name
+end
+
 jst_now = Time.now.localtime('+09:00')
-slack.post(scrapbox_diary_url(jst_now))
+today = Date.new(jst_now.year, jst_now.month, jst_now.day)
+
+slack = Slack::Incoming::Webhooks.new(ENV['SLACK_WEBHOOK_URL'])
+slack.post(scrapbox_diary_url(today))
